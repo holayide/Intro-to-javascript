a990c7bf3455725db30d147f8dc0057a60771b90
olaide <holaryidealabi@gmail.com>
2022-06-22 15:30:15 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nameOf = "Olaide"
+let height = "5'6ft"
+let country = "Nigeria"
+
+    
+    
+alert("Name: " + " " + nameOf + "\n" + "Height: " + " " + height + "\n" + "Country: " + " " + country);
+
